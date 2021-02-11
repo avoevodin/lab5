f463e6e2513f50f962f04e904387eda1ed066b03
Andrey Voevodin <avoevodin8888@gmail.com>
2021-02-11 18:16:02 +0300

    added comments for arguments of draw_koch_curve and made some optimizations

--- koch_curve.py
+++ koch_curve.py
@@ -8,25 +8,34 @@ turtle.speed('fastest')
 def draw_koch_curve(line_length, rec_deep: int):
     """Draw Koch curve fractal.
 
+        Keyword args:
+            line_length -- length of base line (int)
+            rec_deep -- deep of recursion (int)
+
     """
     segment_length = line_length / 3
+    draw_koch_segment(segment_length, rec_deep)
+    turtle.left(60)
+    draw_koch_segment(segment_length, rec_deep)
+    turtle.right(120)
+    draw_koch_segment(segment_length, rec_deep)
+    turtle.left(60)
+    draw_koch_segment(segment_length, rec_deep)
+
+
+def draw_koch_segment(segment_length, rec_deep):
+    """Draw segment of Koch curve.
+
+        Keyword args:
+            segment_length -- length of base line (int)
+            rec_deep -- deep of recursion (int)
+
+    """
     if rec_deep == 1:
         turtle.forward(segment_length)
-        turtle.left(60)
-        turtle.forward(segment_length)
-        turtle.right(120)
-        turtle.forward(segment_length)
-        turtle.left(60)
-        turtle.forward(segment_length)
-        return
-    draw_koch_curve(segment_length, rec_deep - 1)
-    turtle.left(60)
-    draw_koch_curve(segment_length, rec_deep - 1)
-    turtle.right(120)
-    draw_koch_curve(segment_length, rec_deep - 1)
-    turtle.left(60)
-    draw_koch_curve(segment_length, rec_deep - 1)
+    else:
+        draw_koch_curve(segment_length, rec_deep - 1)
 
 
 if __name__ == "__main__":
-    draw_koch_curve(250, 4)
+    draw_koch_curve(250, 2)
